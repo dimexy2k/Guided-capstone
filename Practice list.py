f9bc68c09859684cce3164d17992729ac9fd2061
dimexy2k <77563259+dimexy2k@users.noreply.github.com>
2021-01-18 12:24:07 -0500

    Add files via upload

---
+++ Practice list.py
@@ -0,0 +1,37 @@
+
+#practice list
+
+months = ['January', 'February', 'March']
+
+print(months[1])
+
+message = 'I was born in' + ' ' + months[2]
+
+print(message)
+
+
+#practice list
+
+Years = ['1990', '1991', '1992', '1993']
+
+print(Years)
+
+print(Years[2])
+
+message = 'I was born in year' + ' ' + Years[2]
+
+print(message)
+
+months.append('april')
+
+print(months)
+
+months.sort()
+
+
+print(months)
+
+for month in months:
+    print(month)
+
+print(month + 'name of a month')
